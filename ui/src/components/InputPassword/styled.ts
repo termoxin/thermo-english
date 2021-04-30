e21c2fb7c8c8f7c52faa5d46c45ff893d7cc5370
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2021-04-30 22:32:56 +0300

    Feature: positionate visiblity icon inside input password component

--- ui/src/components/InputPassword/styled.ts
+++ ui/src/components/InputPassword/styled.ts
@@ -1,25 +1,13 @@
 import styled from 'styled-components'
 
-export const BaseInput = styled.input`
-  font-family: 'Rubik', sans-serif;
-  font-weight: 400;
+export const VisibilityIconContainer = styled.div`
+  position: relative;
+  left: 90%;
+  bottom: 39px;
+  cursor: pointer;
+`
 
+export const InputPasswordContainer = styled.div`
   width: 343px;
   height: 53px;
-  box-sizing: border-box;
-  border-radius: 12px;
-  padding-left: 16px;
-
-  background: ${({ theme }) => theme.input.background};
-  color: ${({ theme }) => theme.input.color};
-  border: 1px solid #bebab3;
-  box-shadow: 2px 3px 4px rgba(0, 0, 0, 0.25);
-
-  &::placeholder {
-    color: #78746d;
-  }
-
-  &:focus {
-    outline: none;
-  }
 `
