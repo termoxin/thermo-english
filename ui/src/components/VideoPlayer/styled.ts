ef2df15349d429a843d67504e092cc49cd3af7bd
Rostislav <rostislav.futornoy@gmail.com>
2021-05-02 02:58:26 +0300

    Feature: create video player (#5)

---
+++ ui/src/components/VideoPlayer/styled.ts
@@ -0,0 +1,39 @@
+import styled from 'styled-components'
+
+export const VideoPlayerContainer = styled.div`
+  position: relative;
+  width: 375px;
+  height: 211px;
+
+  video {
+    width: 100%;
+    height: 100%;
+  }
+`
+
+export const Controls = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  svg {
+    z-index: 2;
+    cursor: pointer;
+  }
+`
+
+export const Overlay = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+
+  background: rgba(0, 0, 0, 0.5);
+`
---
+++ ui/src/components/VideoPlayer/video-player.test.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { getByText, render } from '@testing-library/react'
+import { ThemeProvider } from 'styled-components'
+
+import { themes } from '../../themes'
+import { VideoPlayer } from '.'
+import userEvent from '@testing-library/user-event'
+
+describe('VideoPlayer', () => {
+  const props = {
+    src: '',
+  }
+
+  test('should render correctly', () => {
+    const { getByTestId, queryByTestId } = render(
+      <ThemeProvider theme={themes.light}>
+        <VideoPlayer {...props} />
+      </ThemeProvider>,
+    )
+
+    expect(getByTestId('video')).toBeInTheDocument()
+    expect(queryByTestId('controls')).not.toBeInTheDocument()
+    expect(queryByTestId('play-button')).not.toBeInTheDocument()
+  })
+
+  test('overlay and controls should shown', () => {
+    const { getByTestId, queryByTestId } = render(
+      <ThemeProvider theme={themes.light}>
+        <VideoPlayer {...props} />
+      </ThemeProvider>,
+    )
+
+    expect(queryByTestId('controls')).not.toBeInTheDocument()
+    expect(queryByTestId('play-button')).not.toBeInTheDocument()
+    expect(queryByTestId('overlay')).not.toBeInTheDocument()
+
+    userEvent.hover(getByTestId('video-player-container'))
+
+    expect(getByTestId('controls')).toBeInTheDocument()
+    expect(getByTestId('play-button')).toBeInTheDocument()
+    expect(queryByTestId('overlay')).toBeInTheDocument()
+
+    userEvent.unhover(getByTestId('video-player-container'))
+
+    expect(queryByTestId('controls')).not.toBeInTheDocument()
+    expect(queryByTestId('play-button')).not.toBeInTheDocument()
+    expect(queryByTestId('overlay')).not.toBeInTheDocument()
+  })
+})
