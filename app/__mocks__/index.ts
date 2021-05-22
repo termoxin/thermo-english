bb00bfaeffd6636326a1ccf93f9826fee55b100b
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2021-05-22 13:56:55 +0300

    Refactor: remove redundant filters

--- app/__mocks__/index.ts
+++ app/__mocks__/index.ts
@@ -55,6 +55,4 @@ export const postFilters = [
   { label: 'Popular', value: 'popular' },
   { label: 'Recent', value: 'recent' },
   { label: 'Oldest', value: 'oldest' },
-  { label: 'Most viewed', value: 'most-viewed' },
-  { label: 'Most liked', value: 'most-liked' },
 ]
