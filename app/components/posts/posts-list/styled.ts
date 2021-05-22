51c7aae1089431536d204da0d19e10e4d91bc888
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2021-05-22 13:56:45 +0300

    Feature: use grid instead of flexbox to build grid

--- app/components/posts/posts-list/styled.ts
+++ app/components/posts/posts-list/styled.ts
@@ -1,24 +1,15 @@
 import styled from 'styled-components'
-import { PostCardContainer, breakpoints } from 'ui'
+import { breakpoints } from 'ui'
 
 export const PostsListWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  flex-wrap: wrap;
-
-  ${PostCardContainer} {
-    margin: 2rem 0.5rem 3rem 0.5rem;
-  }
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, 37rem);
+  justify-content: space-around;
+  row-gap: 3rem;
+  column-gap: 1rem;
 
   ${breakpoints.media.desktop`
     justify-content: space-around;
-  `}
-
-  ${breakpoints.media.smallDesktop`
-    justify-content: center;
-  `}
-
-  ${breakpoints.media.tablet`
-    justify-content: center;
   `}
 `
