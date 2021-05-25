70e1c165ec45cbe9bcbfcbe221940ff924c0834d
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2021-05-25 20:16:21 +0300

    Feature: toggle theme by click on app logo

--- app/components/layout-authenticated/index.tsx
+++ app/components/layout-authenticated/index.tsx
@@ -1,12 +1,16 @@
 import { FC } from 'react'
 import { Logo, Button } from 'ui'
+import { LayoutAuthenticatedProps } from './layout-authenticated.types'
 
 import { NavigationContainer, Actions, Dots } from './styled'
 
-export const LayoutAuthenticated: FC = ({ children }) => (
+export const LayoutAuthenticated: FC<LayoutAuthenticatedProps> = ({
+  children,
+  toggleTheme,
+}) => (
   <div>
     <NavigationContainer>
-      <Logo width="201" height="125" />
+      <Logo width="201" height="125" onClick={toggleTheme} />
       <Actions>
         <Button variant="squared">Request post</Button>
         <Button variant="squared">Admin panel</Button>
--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -27,7 +27,7 @@ const App = ({ Component, pageProps }: AppProps) => {
         {guestRoutes.includes(router.route) ? (
           <Component {...currentPageProps} />
         ) : (
-          <LayoutAuthenticated>
+          <LayoutAuthenticated toggleTheme={toggleTheme}>
             <Component {...currentPageProps} />
           </LayoutAuthenticated>
         )}
