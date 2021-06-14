2c05d8f39e2223f41cfa97040d7db6807d5dcb6c
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2021-06-14 05:15:26 +0300

    Style: design post item

--- ui/src/components/PostItem/post-item.tsx
+++ ui/src/components/PostItem/post-item.tsx
@@ -5,7 +5,14 @@ import PencilIcon from '../../icons/Pencil'
 import TickIcon from '../../icons/Tick'
 
 import { PostItemProps } from './post-item.types'
-import { Preview, PostItemContainer } from './styled'
+import {
+  Preview,
+  PostItemContainer,
+  LikesBlock,
+  IconsBlock,
+  ContentBlock,
+  LikesAndTextBlock,
+} from './styled'
 
 export const PostItem: FC<PostItemProps> = ({
   text,
@@ -18,26 +25,36 @@ export const PostItem: FC<PostItemProps> = ({
 }) => (
   <PostItemContainer>
     <Preview src={previewUrl} alt={text} />
-    <div>
-      <p>{text}</p>
-      <p>{likes} likes</p>
-    </div>
+    <ContentBlock>
+      <LikesAndTextBlock>
+        <p>{text}</p>
+        {state === 'draft' ||
+          (state === 'published' && <LikesBlock>{likes} likes</LikesBlock>)}
+      </LikesAndTextBlock>
+      <IconsBlock>
+        {state === 'requested' ? (
+          <TickIcon
+            data-testid="accept-post-button"
+            width="14"
+            height="14"
+            onClick={() => onAcceptPost && onAcceptPost()}
+          />
+        ) : (
+          <PencilIcon
+            data-testid="edit-post-button"
+            width="14"
+            height="14"
+            onClick={() => onEditPost && onEditPost()}
+          />
+        )}
 
-    {state === 'requested' ? (
-      <TickIcon
-        data-testid="accept-post-button"
-        onClick={() => onAcceptPost && onAcceptPost()}
-      />
-    ) : (
-      <PencilIcon
-        data-testid="edit-post-button"
-        onClick={() => onEditPost && onEditPost()}
-      />
-    )}
-
-    <CrossIcon
-      data-testid="reject-post-button"
-      onClick={() => onRejectPost && onRejectPost()}
-    />
+        <CrossIcon
+          data-testid="reject-post-button"
+          width="14"
+          height="14"
+          onClick={() => onRejectPost && onRejectPost()}
+        />
+      </IconsBlock>
+    </ContentBlock>
   </PostItemContainer>
 )
--- ui/src/components/PostItem/styled.ts
+++ ui/src/components/PostItem/styled.ts
@@ -2,12 +2,53 @@ import styled from 'styled-components'
 
 export const PostItemContainer = styled.div`
   display: flex;
-  background: #454d58;
+  padding: 0.8rem 1.5rem 0.8rem 0.8rem;
   height: 7rem;
   width: 34rem;
+  font-size: 1.6rem;
+  border-radius: 0.8rem;
+
+  background: ${({ theme }) => theme.postItem.background};
+  color: ${({ theme }) => theme.postItem.color};
+
+  svg {
+    cursor: pointer;
+  }
+`
+
+export const LikesBlock = styled.div`
+  background: ${({ theme }) => theme.postItem.likesBlock.background};
+  color: ${({ theme }) => theme.postItem.likesBlock.color};
+  border-radius: 0.8rem;
+  padding: 0.3rem 0.8rem;
+  font-size: 1.4rem;
+  width: max-content;
+`
+
+export const LikesAndTextBlock = styled.div`
+  margin-left: 2rem;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`
+
+export const IconsBlock = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 4.1rem;
+`
+
+export const ContentBlock = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
 `
 
 export const Preview = styled.img`
-  height: 5.8rem;
   width: 5.8rem;
+  height: auto;
+
+  border-radius: 0.8rem;
 `
