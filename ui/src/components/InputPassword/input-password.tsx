0172ee219cbe18874b4832893df3ff7fd4af407f
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2021-06-27 13:28:46 +0300

    Feature: forward ref in input password component

--- ui/src/components/InputPassword/input-password.tsx
+++ ui/src/components/InputPassword/input-password.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react'
+import React, { forwardRef, useState } from 'react'
 
 import { Input } from '../Input'
 import CrossedEye from '../../icons/CrossedEye'
@@ -7,36 +7,39 @@ import Eye from '../../icons/Eye'
 import { InputPasswordProps } from './input-password.types'
 import { VisibilityIconContainer, InputPasswordContainer } from './styled'
 
-export const InputPassword: FC<InputPasswordProps> = (props) => {
-  const [showPassword, setShowPassword] = useState(false)
+export const InputPassword = forwardRef<HTMLInputElement, InputPasswordProps>(
+  (props, ref) => {
+    const [showPassword, setShowPassword] = useState(false)
 
-  const inputType = showPassword ? 'text' : 'password'
+    const inputType = showPassword ? 'text' : 'password'
 
-  return (
-    <InputPasswordContainer>
-      <Input
-        {...props}
-        type={inputType}
-        data-testid="password-input"
-        role="password-input"
-      />
-      <VisibilityIconContainer>
-        {showPassword ? (
-          <Eye
-            height="20"
-            width="20"
-            onClick={() => setShowPassword(false)}
-            data-testid="hide-password"
-          />
-        ) : (
-          <CrossedEye
-            height="20"
-            width="20"
-            onClick={() => setShowPassword(true)}
-            data-testid="show-password"
-          />
-        )}
-      </VisibilityIconContainer>
-    </InputPasswordContainer>
-  )
-}
+    return (
+      <InputPasswordContainer>
+        <Input
+          {...props}
+          type={inputType}
+          data-testid="password-input"
+          role="password-input"
+          ref={ref}
+        />
+        <VisibilityIconContainer>
+          {showPassword ? (
+            <Eye
+              height="20"
+              width="20"
+              onClick={() => setShowPassword(false)}
+              data-testid="hide-password"
+            />
+          ) : (
+            <CrossedEye
+              height="20"
+              width="20"
+              onClick={() => setShowPassword(true)}
+              data-testid="show-password"
+            />
+          )}
+        </VisibilityIconContainer>
+      </InputPasswordContainer>
+    )
+  },
+)
